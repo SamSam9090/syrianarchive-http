45edd35a59e160b7b0c742617ab62f3307a74893
niko <niko@syrianarchive.org>
2017-11-28 16:03:45 +0100

    set filters obvs

--- app/js/components/ListEvidence.js
+++ app/js/components/ListEvidence.js
@@ -13,7 +13,7 @@ export default class ListEvidence extends Component {
           </small>
         </div>
         <div className="col-2">{i.incident_date}</div>
-        <div className="col-6">{unitTitle(i)}</div>
+        <div className="col-6 dtitle">{unitTitle(i)}</div>
         <div className="col-2">{location(i.location)}</div>
         <div className="col-1">
           <button className="btn" onClick={this.props.selector}>
