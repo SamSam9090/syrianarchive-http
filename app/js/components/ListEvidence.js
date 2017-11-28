eb89dd77090dc2bca892488627034851fef0153d
niko <niko@syrianarchive.org>
2017-11-28 18:17:51 +0100

    switch columns

--- app/js/components/ListEvidence.js
+++ app/js/components/ListEvidence.js
@@ -7,13 +7,13 @@ export default class ListEvidence extends Component {
     const i = this.props.unit;
     return (
       <div className="columns item">
+        <div className="col-1"><small>{i.incident_date}</small></div>
+        <div className="col-7 dtitle"><span>{unitTitle(i)}</span></div>
         <div className="col-1">
           <small>
             {i.reference_code}
           </small>
         </div>
-        <div className="col-1"><small>{i.incident_date}</small></div>
-        <div className="col-7 dtitle"><span>{unitTitle(i)}</span></div>
         <div className="col-2"><small>{location(i.location)}</small></div>
         <div className="col-1">
           <button className="btn" onClick={this.props.selector}>
