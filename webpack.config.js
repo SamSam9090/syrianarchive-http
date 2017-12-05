63ee9e9f19d36ecffa0a9420753259a430e82f9b
niko <niko@syrianarchive.org>
2017-12-05 14:04:48 +0100

    removed uglify till it works

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, './app'),
@@ -49,6 +49,5 @@ module.exports = {
       allChunks: true,
     }),
     new CopyWebpackPlugin([{ from: '../assets' }]),
-    new UglifyJsPlugin(),
   ],
 };
